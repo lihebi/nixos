a9a97097e98f7306dc27319f14ba6c72497b8234
Hebi Li <lihebi.com@gmail.com>
2019-11-21 14:13:08 -0600

    init

---
+++ configuration.nix
@@ -0,0 +1,126 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.nixPath = [
+    "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+    "nixos-config=/home/hebi/git/nixos/configuration.nix" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # this works for WM and chrome, but does not seem to work for xterm and urxvt
+  services.xserver.dpi = 144;
+  fonts.fontconfig.dpi = 144;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+
+  networking.networkmanager.enable = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget git emacs vim racket rxvt_unicode tmux
+    sbcl silver-searcher
+    # FIXME where did the package go? If installed locally via nix-env -iA, they
+    # are available in ~/.nix-profile/lib/common-lisp/, but when about globally?
+    # It is /run/current-system/sw/lib/common-lisp, where sw stands for
+    # system-path?
+    #
+    lispPackages.clx-truetype
+    lispPackages.clwrapper
+    # lispPackages.fset
+    #
+    # OK, I'm going to use quicklisp. I can quicklisp init and quicklisp install
+    # clx-truetype, but how to (require :clx-truetype)? Where did it install? It
+    # is ~/quicklisp/
+    # lispPackages.quicklisp
+    # xdpyinfo
+    # chromium
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.displayManager.slim.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.windowManager.stumpwm.enable = true;
+  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  # services.xserver.windowManager.sway.enable = true;
+  programs.sway.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hebi = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+
+}
+
