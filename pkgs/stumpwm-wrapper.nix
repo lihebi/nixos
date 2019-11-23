28454e022092de100940265406f59ffd1335330a
Hebi Li <lihebi.com@gmail.com>
2019-11-23 17:54:27 -0600

    clean up stumpwm-wrapper.nix and add notes

--- pkgs/stumpwm-wrapper.nix
+++ pkgs/stumpwm-wrapper.nix
@@ -1,25 +1,4 @@
 with import <nixpkgs> {};
-# { stdenv, pkgs, ... }:
-
-# FIXME my stumpwm to add clx-truetype
-#
-# let
-#   callPackage = pkgs.lib.callPackageWith quicklisp-to-nix-packages;
-#   buildLispPackage = callPackage ./define-package.nix;
-#   qlOverrides = callPackage ./quicklisp-to-nix-overrides.nix {};
-#   "myStumpwm" = buildLispPackage
-#     ((f: x: (x // (f x)))
-#        (qlOverrides."stumpwm" or (x: {}))
-#        (import ./quicklisp-to-nix-output/stumpwm.nix {
-#          inherit fetchurl;
-#            "alexandria" = quicklisp-to-nix-packages."alexandria";
-#            "cl-ppcre" = quicklisp-to-nix-packages."cl-ppcre";
-#            "clx" = quicklisp-to-nix-packages."clx";
-#            "clx-truetype" = quicklisp-to-nix-packages."clx-truetype";
-#        }));
-# in
-
-# Or, just add clx-truetype to shell-config.sh
 
 stdenv.mkDerivation rec {
   pname = "stumpwm-wrapper";
@@ -30,10 +9,6 @@ stdenv.mkDerivation rec {
                   pkgs.lispPackages.clwrapper
                   pkgs.lispPackages.swank
                   pkgs.lispPackages.clx-truetype ];
-
-  #   (import <nixpkgs> {}).writeScript("stumpwm-wrapper.sh", ''
-  # THE CONTENT
-  # '')
 
   buildPhase = ''
   '';
@@ -61,17 +36,4 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.hebi ];
     platforms = platforms.unix;
   };
-  # propagatedBuildInputs
 }
-
-  # config_script=
-  # echo "source '$config_script'" >> "$script"
-
-  # Actually it does not seem to matter
-  # echo "export PATH=\"\$PATH:"${pkgs.lispPackages.clx-truetype}/bin\" >> "$script"
-
-  # but this matters, NIX_LISP_ASDF_PATHS
-  # echo "export NIX_LISP_ASDF_PATHS=\"\$NIX_LISP_ASDF_PATHS ${pkgs.lispPackages.clx-truetype}/lib/common-lisp/clx-truetype\"" >> "$script"
-  # echo 'echo $NIX_LISP_ASDF_PATHS' >> "$script"
-
-  # echo '"${pkgs.lispPackages.clwrapper}/bin/common-lisp.sh" --quit --eval "(require :clx-truetype)"' >> "$script"
