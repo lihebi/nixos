28bdb0da743ff1d016ea898809d0be69e771d620
Hebi Li <lihebi.com@gmail.com>
2021-06-27 18:58:12 -0500

    test scripts

---
+++ shell.nix
@@ -0,0 +1,23 @@
+
+let
+  jupyter = import (builtins.fetchGit {
+    url = https://github.com/tweag/jupyterWith;
+    rev = "";
+  }) {};
+
+  iPython = jupyter.kernels.iPythonWith {
+    name = "python";
+    packages = p: with p; [ numpy ];
+  };
+
+  iHaskell = jupyter.kernels.iHaskellWith {
+    name = "haskell";
+    packages = p: with p; [ hvega formatting ];
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iPython iHaskell ];
+    };
+in
+jupyterEnvironment.env
